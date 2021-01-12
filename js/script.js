591f1410034a5051cf8880592950f0976da25215
Rakler <35872691+Rakler@users.noreply.github.com>
2021-01-12 19:47:28 +0000

    addedThemeAndColorSelectionFunctionality

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 //Instantiate constants that we are gonna be using throughout the project
 const nameInput = document.getElementById("name");
 const otherJob = document.getElementById("other-job-role");
+const colorSelect = document.getElementById("shirt-colors");
+const designSelect = document.getElementById("design");
 
 //Set focus on "Name" input element
 nameInput.focus;
@@ -18,5 +20,31 @@ jobRoleSelect.addEventListener("change", function(e){
     if(e.target.value === "other"){
         console.log(e.target.value);
         otherJob.style.display = "block";
+    }
+});
+
+colorSelect.style.display = "none";
+
+designSelect.addEventListener("change", function(e){
+    if(e.target.value === "js puns"){
+        colorSelect.style.display = "block";
+        colorSelect.children[1].children[0].style.display = "none";
+        colorSelect.children[1].children[1].style.display = "block";
+        colorSelect.children[1].children[2].style.display = "block";
+        colorSelect.children[1].children[3].style.display = "block";
+
+        colorSelect.children[1].children[4].style.display = "none";
+        colorSelect.children[1].children[5].style.display = "none";
+        colorSelect.children[1].children[6].style.display = "none";
+    } else if(e.target.value === "heart js"){
+        colorSelect.style.display = "block";
+        colorSelect.children[1].children[0].style.display = "none";
+        colorSelect.children[1].children[1].style.display = "none";
+        colorSelect.children[1].children[2].style.display = "none";
+        colorSelect.children[1].children[3].style.display = "none";
+
+        colorSelect.children[1].children[4].style.display = "block";
+        colorSelect.children[1].children[5].style.display = "block";
+        colorSelect.children[1].children[6].style.display = "block";
     }
 });
