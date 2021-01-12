7c8141853b86fa7453bb3ee11ae47a5e2ec81c98
Rakler <35872691+Rakler@users.noreply.github.com>
2021-01-12 20:19:21 +0000

    addedCheckboxFunctionality

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const nameInput = document.getElementById("name");
 const otherJob = document.getElementById("other-job-role");
 const colorSelect = document.getElementById("shirt-colors");
 const designSelect = document.getElementById("design");
-
+const activities = document.getElementById("activities");
+const activitiesCost = document.getElementById("activities-cost");
+var cost = 0;
 //Set focus on "Name" input element
 nameInput.focus;
 
@@ -48,3 +50,27 @@ designSelect.addEventListener("change", function(e){
         colorSelect.children[1].children[6].style.display = "block";
     }
 });
+
+activities.addEventListener("change", function(e){
+    if(e.target.nodeName === "INPUT" && e.target.checked === true){
+        addCost(e.target);
+
+        console.log(activitiesCost.textContent);
+
+    } else if(e.target.nodeName === "INPUT" && e.target.checked === false){
+        substractCost(e.target);
+        console.log(activitiesCost.textContent);
+    }
+});
+
+//Function for adding the cost for each of the checkboxes
+var addCost = function(element){
+    cost += parseInt(element.getAttribute("data-cost"));
+    activitiesCost.textContent = "Total: $" + cost;
+}
+
+//Function for substracting the cost as each checkbox gets unchecked.
+var substractCost = function(element){
+    cost -= parseInt(element.getAttribute("data-cost"));
+    activitiesCost.textContent = "Total: $" + cost;
+}
