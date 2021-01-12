d99fda817fa339dfc2d48c354fe004e5cd6bccbb
Rakler <35872691+Rakler@users.noreply.github.com>
2021-01-12 21:12:47 +0000

    startedWorkOnPaymentMethods

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ const colorSelect = document.getElementById("shirt-colors");
 const designSelect = document.getElementById("design");
 const activities = document.getElementById("activities");
 const activitiesCost = document.getElementById("activities-cost");
+
+const paymentSelect = document.querySelector(".payment-methods");
+const paypal = document.getElementById("paypal");
+const bitcoin = document.getElementById("bitcoin");
 var cost = 0;
 //Set focus on "Name" input element
 nameInput.focus;
@@ -74,3 +78,20 @@ var substractCost = function(element){
     cost -= parseInt(element.getAttribute("data-cost"));
     activitiesCost.textContent = "Total: $" + cost;
 }
+paymentSelect.children[1].addEventListener("change", function(e){
+    
+    if(e.target.value === "paypal"){
+        console.log(e.target);
+        paymentSelect.children[2].style.display = "none";
+    }
+});
+
+//Set Credit Card selected as the default value
+paymentSelect.children[1].children[1].children[1].selected = true;
+
+paymentSelect.children[3].style.display = "none";
+//Hide Paypal and Bitcoin initially as Credit Card is selected.
+// paypal.style.display = "none";
+// bitcoin.style.display = "none";
+
+
